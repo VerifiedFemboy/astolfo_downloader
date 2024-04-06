30120e036a36a7e103c49420a969262c00c602c2
verifiedfemboy <maciekplaygamesyt@gmail.com>
2024-04-06 15:50:17 +0200

    Configuration File

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include "fstream"
 #include "request.h"
 
 using rq = rqst::request;
 using namespace std;
 
 int main() {
+
+    ifstream file("settings.json");
+    if(!file.good()) {
+        JSON defaultSettings = {
+                {"console_only", true},
+                {"nsfw", false}
+        };
+
+        ofstream out("settings.json");
+        out << setw(4) << defaultSettings;
+
+        cout << "Created Configuration File" << endl;
+    }
+
     JSON read = rq::send("https://astolfo.poligon.lgbt/api/sfw");
     read.dump(4);
     cout << read["url"];
